066b78704ca331191d4f076fd2d5cdea34a529e5
NathanC16 <nathancfranca.com>
2025-10-21 18:30:44 -0300

    teste

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
+    './index.html',
+    './src/App.vue',
+    './src/components/**/*.vue',
   ],
   theme: {
     extend: {},
