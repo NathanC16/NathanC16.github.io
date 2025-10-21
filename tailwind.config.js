1816079cdcfb9fadedcc0995f9ad516242e081ff
NathanC16 <nathancfranca.com>
2025-10-21 18:15:54 -0300

    teste

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './App.tsx',
-    './index.tsx',
-    './components/Footer.tsx',
-    './components/Header.tsx',
-    './components/LanguageSwitcher.tsx',
-    './components/Preloader.tsx',
-    './components/ProjectCard.tsx',
-    './components/ProjectList.tsx',
-    './components/icons/FolderIcon.tsx',
-    './components/icons/GithubIcon.tsx',
-    './components/icons/LinkIcon.tsx',
-    './contexts/LanguageContext.tsx',
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
